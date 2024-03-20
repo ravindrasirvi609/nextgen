39479f0d2ae93a603154e985eb803dd6b655d9a2
Ravindra <sirviravindra609@gmail.com>
2024-03-20 22:29:07 +0530

    Update title and description, fix NEXTGen spelling, add GlobeDemo component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 "use client";
 import FeaturedBlogs from "@/components/FeaturedBlogs";
 import HeroSection from "@/components/HeroSection";
+import { GlobeDemo } from "@/components/global";
 import { Navbar } from "@/components/navbar";
 import { BackgroundGradient } from "@/components/ui/background-gradient";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       {/* <Image src="/nextgen.png" alt="NEXTGen" width={300} height={300} /> */}
       <HeroSection />
       <FeaturedBlogs />
+      <div className="w-full my-8">
+        <GlobeDemo />
+
+      </div>
+
     </main>
   );
 }

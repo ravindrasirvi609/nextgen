2479d7110d7269ce74e8c1880ac20ecd51cf1c92
Ravindra <sirviravindra609@gmail.com>
2024-04-09 07:43:54 +0530

    Refactor Testimonials page to fetch testimonials from API

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -1,37 +1,48 @@
-import React from 'react';
+"use client";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 const Testimonials = () => {
-      const testimonials = [
-            {
-                  id: 1,
-                  name: 'John Doe',
-                  quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            },
-            {
-                  id: 2,
-                  name: 'Jane Smith',
-                  quote: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem.',
-            },
-            {
-                  id: 3,
-                  name: 'Mike Johnson',
-                  quote: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.',
-            },
-      ];
+  const [testimonials, setTestimonials] = useState([]);
 
-      return (
-            <div className="container mx-auto py-44">
-                  <h2 className="text-2xl font-bold mb-4">Testimonials</h2>
-                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                        {testimonials.map((testimonial) => (
-                              <div key={testimonial.id} className="bg-white p-4 shadow rounded">
-                                    <p className="text-gray-800 mb-2">{testimonial.quote}</p>
-                                    <p className="text-gray-600">{testimonial.name}</p>
-                              </div>
-                        ))}
-                  </div>
-            </div>
-      );
+  useEffect(() => {
+    fetchTestimonials();
+  }, []);
+
+  const fetchTestimonials = async () => {
+    try {
+      const response = await axios.get("/api/testimonial/testimonial-list");
+      setTestimonials(response.data.data);
+    } catch (error) {
+      console.error("Error fetching testimonials:", error);
+    }
+  };
+
+  return (
+    <div className="container mx-auto py-40">
+      <h2 className="text-3xl font-bold mb-8 text-center">Testimonials</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {testimonials.map(
+          (testimonial: { message: string; name: string; _id: string }) => (
+            <motion.div
+              key={testimonial._id}
+              className="bg-white p-6 rounded overflow-hidden shadow-lg"
+              whileHover={{
+                scale: 1.05,
+                boxShadow: "0px 5px 20px rgba(0, 0, 0, 0.1)",
+              }}
+              whileTap={{ scale: 0.95 }}
+              transition={{ duration: 0.2 }}
+            >
+              <p className="text-gray-800 mb-4">{testimonial.message}</p>
+              <p className="text-gray-600">{testimonial.name}</p>
+            </motion.div>
+          )
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;

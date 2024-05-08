8f181e9f36215206b3127594a6ada1e5a6b0c781
Ravindra <sirviravindra609@gmail.com>
2024-05-08 10:50:04 +0530

    refactor: Fetch leader details from API in LeadersDetails component

--- src/app/leadership/[id]/page.tsx
+++ src/app/leadership/[id]/page.tsx
@@ -1,20 +1,37 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Image from "next/image";
+import axios from "axios";
 
-const LeadersDetails: React.FC = () => {
-  const leader = {
-    id: 1,
-    name: "John Doe",
-    position: "Chief Executive Officer",
-    bio: "John Doe was born on 30 November 1858, in Myemsingh, Faridpur, a part of the Dhaka District now in Bangladesh. He attended the village school until he was 11. He then moved to Kolkata where he enrolled in St. Xavier’s. He was very much interested in Biology. However, Father Lafont, a famous Professor of Physics, inspired in Bose a great interest in Physics. Having obtained his B.A. in physical sciences, twenty-two-year-old Bose left for London to obtain a medical degree. However, he kept falling ill and had to discontinue his plans to be a doctor. He then obtained his B.A. degree from Christ College, Cambridge. He returned to India in 1885 and joined Presidency College, Kolkata as an Assistant Professor of Physics, where he remained until 1915. There was a peculiar practice in the college at that time. The Indian teachers in the college were paid one-third of what the British teachers were paid! So Bose refused his salary but worked for three years. The fourth year he was paid in full! He was an excellent teacher, extensively using scientific demonstrations in class. Some of his students, such as S. N. Bose, went on to become famous physicists themselves. During this period, Bose also started doing original scientific work in the area of microwaves, carrying out experiments involving refraction, diffraction, and polarization. He developed the use of galena crystals for making receivers, both for short-wavelength radio waves and for white and ultraviolet light. In 1895, two years before Marconi’s demonstration, Bose demonstrated wireless communication using radio waves, using them to ring a bell remotely and to explode some gunpowder. Many of the microwave components familiar today - waveguides, horn antennas, polarizers, dielectric lenses and prisms, and even semiconductor detectors of electromagnetic radiation - were invented and used by Bose in the last decade of the nineteenth century. He also suggested the existence of electromagnetic radiation from the Sun, which was confirmed in 1944. Bose then turned his attention to response phenomena in plants. He showed that not only animal but vegetable tissues produce similar electric responses under different kinds of stimuli – mechanical, thermal, electrical, and chemical. Bose was knighted in 1917 and soon thereafter elected Fellow of the Royal Society, London, (both as physicist and biologist!). Bose had worked all along without the right kind of scientific instruments and laboratory. For a long time, he had been thinking of building a laboratory. The result was the establishment of the Bose Research Institute in Kolkata. It continues to be a famous center of research in basic sciences.",
-    imageUrl:
-      "https://plus.unsplash.com/premium_photo-1678197937465-bdbc4ed95815?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-    email: "ravi.sirvi609@gmail.com",
-    phone: "+91 1234567890",
-    location: "New York, USA",
-    education: "MBA, Harvard Business School",
-    linkedin: "https://www.linkedin.com/in/johndoe",
-  };
+const LeadersDetails: React.FC = ({ params }: any) => {
+  const id = params.id;
+  const [leader, setLeader] = React.useState({
+    _id: "",
+    name: "",
+    position: "",
+    bio: "",
+    email: "",
+    phone: "",
+    location: "",
+    education: "",
+    linkedin: "",
+    imageUrl: "",
+  });
+  useEffect(() => {
+    const fetchLeader = async () => {
+      try {
+        const response = await axios.post(`/api/leaders/leader-details`, {
+          id,
+        });
+        const data = await response.data;
+        setLeader(data.leader);
+      } catch (error) {
+        console.error("Error fetching leader:", error);
+      }
+    };
+
+    fetchLeader();
+  }, [id]);
 
   return (
     <div className="bg-gray-100 py-40 px-4 sm:px-6 lg:px-8">
--- src/app/leadership/page.tsx
+++ src/app/leadership/page.tsx
@@ -4,55 +4,27 @@ import Image from "next/image";
 import Link from "next/link";
 import axios from "axios";
 
-const Leadership = () => {
-  const leaders = [
-    {
-      id: "1",
-      name: "John Doe",
-      position: "CEO",
-      bio: "John Doe is the founder and CEO of NextGEN Leaders. With over 20 years of experience in leadership and business management, John has led the company to unprecedented growth and success. He is passionate about empowering the next generation of leaders and fostering innovation. Prior to founding NextGEN Leaders, John held executive positions at several Fortune 500 companies.",
-      imageUrl:
-        "https://images.unsplash.com/photo-1599566150163-29194dcaad36?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-      email: "john.doe@example.com",
-      phone: "+1234567890",
-      location: "New York, USA",
-      education: "MBA, Harvard Business School",
-      linkedin: "https://www.linkedin.com/in/johndoe",
-    },
-    {
-      id: "2",
-      name: "Jane Smith",
-      position: "CTO",
-      bio: "Jane Smith is the Chief Technology Officer at NextGEN Leaders. She leads the technology team with her expertise in software development and product management. Jane is dedicated to driving technological innovation and ensuring the highest standards of quality in our products. Prior to joining NextGEN Leaders, Jane worked as a technology consultant for top-tier tech firms.",
-      imageUrl:
-        "https://plus.unsplash.com/premium_photo-1678197937465-bdbc4ed95815?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-      email: "jane.smith@example.com",
-      phone: "+1987654321",
-      location: "San Francisco, USA",
-      education: "BSc in Computer Science, Stanford University",
-      linkedin: "https://www.linkedin.com/in/janesmith",
-    },
-    {
-      id: "3",
-      name: "Michael Johnson",
-      position: "COO",
-      bio: "Michael Johnson serves as the Chief Operating Officer at NextGEN Leaders. With a background in operations management and strategic planning, Michael oversees the day-to-day operations of the company. He is committed to operational excellence and ensuring the efficient delivery of our services. Michael holds a Bachelor's degree in Business Administration from University of California, Berkeley.",
-      imageUrl:
-        "https://images.unsplash.com/photo-1580489944761-15a19d654956?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D",
-      email: "michael.johnson@example.com",
-      phone: "+1122334455",
-      location: "Los Angeles, USA",
-      education: "BS in Management, University of California, Berkeley",
-      linkedin: "https://www.linkedin.com/in/michaeljohnson",
-    },
-  ];
+interface Leader {
+  _id: string;
+  name: string;
+  position: string;
+  bio: string;
+  email: string;
+  phone: string;
+  location: string;
+  education: string;
+  linkedin: string;
+  imageUrl: string;
+}
 
+const Leadership = () => {
+  const [leaders, setLeaders] = React.useState([]);
   useEffect(() => {
     const fetchLeaders = async () => {
       try {
         const response = await axios.get("/api/leaders/leaders-list");
         const data = await response.data;
-        console.log("Leaders data:", data);
+        setLeaders(data.leaders);
       } catch (error) {
         console.error("Error fetching leaders:", error);
       }
@@ -67,10 +39,10 @@ const Leadership = () => {
         NextGEN Leaders
       </h1>
       <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {leaders.map((leader) => (
-          <Link href={`/leadership/${leader.id}`} key={leader.id}>
+        {leaders.map((leader: Leader) => (
+          <Link href={`/leadership/${leader._id}`} key={leader._id}>
             <div
-              key={leader.id}
+              key={leader._id}
               className="bg-white rounded-lg shadow-md overflow-hidden transition duration-300 transform hover:scale-105 hover:shadow-lg"
             >
               <Image

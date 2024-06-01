585121b41c4641bdb05905e9e405ba441bfd3efd
Ravindra <sirviravindra609@gmail.com>
2024-06-01 08:01:20 +0530

    refactor: Update NextSeo component and page structure for improved SEO optimization and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,44 @@ import { NextSeo } from "next-seo";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <>
       <NextSeo
         title="Nexgen Leaders - Your Source for Startup and Pharma News"
         description="Stay updated with the latest news and insights on startups and the pharmaceutical industry. Nexgen Leaders brings you in-depth articles, trends, and analysis."
+        canonical="https://www.nextgenleaders.vip"
+        openGraph={{
+          url: "https://www.nextgenleaders.vip",
+          title: "Nexgen Leaders - Your Source for Startup and Pharma News",
+          description:
+            "Stay updated with the latest news and insights on startups and the pharmaceutical industry. Nexgen Leaders brings you in-depth articles, trends, and analysis.",
+          images: [
+            {
+              url: "https://https://www.nextgenleaders.vip/images/og-image.jpg",
+              width: 800,
+              height: 600,
+              alt: "Nexgen Leaders",
+            },
+          ],
+          site_name: "Nexgen Leaders",
+        }}
+        twitter={{
+          handle: "@nexgenleaders",
+          site: "@nexgenleaders",
+          cardType: "summary_large_image",
+        }}
       />
-      {/* <Image src="/nextgen.png" alt="NEXTGen" width={300} height={300} /> */}
-      {/* <HeroSection /> */}
-      <MacbookScrollDemo />
-      <div className="w-full">
-        <GlobeDemo />
-        <GoogleGeminiEffectDemo />
-        <InfiniteMovingCardsDemo />
-        <FeaturedBlogs />
-      </div>
-    </main>
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        {/* <Image src="/nextgen.png" alt="NEXTGen" width={300} height={300} /> */}
+        {/* <HeroSection /> */}
+        <MacbookScrollDemo />
+        <div className="w-full">
+          <GlobeDemo />
+          <GoogleGeminiEffectDemo />
+          <InfiniteMovingCardsDemo />
+          <FeaturedBlogs />
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 }
